d1fdb69a3e992e193e066495978c292232b88327
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-09-25 16:07:46 +0530

    testing loader

--- src/Components/HeaderMovies/HeaderMovies.js
+++ src/Components/HeaderMovies/HeaderMovies.js
@@ -4,9 +4,11 @@ import axios from 'axios';
 import './HeaderMovies.css';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
+import { Audio } from 'react-loader-spinner'
 
 function HeaderMovies({ GetSelectedMovie }) {
     const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const fetchMovies = async () => {
         const response = await axios.get('https://api.themoviedb.org/3/movie/top_rated', {
@@ -18,6 +20,7 @@ function HeaderMovies({ GetSelectedMovie }) {
         });
         console.log(response.data);
         setMovies(response.data.results);
+        setLoading(false);
     };
 
     useEffect(() => {
@@ -31,12 +34,23 @@ function HeaderMovies({ GetSelectedMovie }) {
     ));
 
     return (
-        <Splide
-            options={{ rewind: true, perPage: 1, gap: '1rem' }}
-            aria-label="Top Rated Movies"
-        >
-            {renderMovies}
-        </Splide>
+        <>
+            {loading && <Audio
+                height="80"
+                width="80"
+                radius="9"
+                color="green"
+                ariaLabel="loading"
+                wrapperStyle
+                wrapperClass
+            />}
+
+            {!loading && <Splide
+                options={{ rewind: true, perPage: 1, gap: '1rem' }}
+                aria-label="Top Rated Movies">
+                {renderMovies}
+            </Splide>}
+        </>
     );
 }
 
